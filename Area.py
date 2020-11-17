4e5f601744cb963196c93f70f4b59224a73cb2ca
Visarathi <70250592+Visarathi@users.noreply.github.com>
2020-11-17 14:12:35 +0530

    Area.py
    This is a program that accepts the radius of a circle from the user and finds the area of the circle.

---
+++ Area.py
@@ -0,0 +1,14 @@
+#finding the Area of the circle:
+
+
+import math
+
+r = float(input("enter the radius of the circle"))
+
+area = math.pi * r *r
+
+print("The radius of the circle is %.2f"%area)
+
+
+
+
